98268ae20ead34c03e8e6f137754e46a56b1efb4
kukoza <gglike21@gmail.com>
2025-02-19 11:46:09 +0700

    update 1146

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,4 +12,43 @@ class UserController extends Controller
     public function User2(Request $request){     
         return view('user2');
     }
+    public function User3(Request $request){     
+        return view('user3');
+    }
+    public function User4(Request $request){     
+        return view('user4');
+    }
+    public function User5(Request $request){     
+        return view('user5');
+    }
+    public function User6(Request $request){     
+        return view('user6');
+    }
+    public function User7(Request $request){     
+        return view('user7');
+    }
+    public function User8(Request $request){     
+        return view('user8');
+    }
+    public function User9(Request $request){     
+        return view('user9');
+    }
+    public function User10(Request $request){     
+        return view('user10');
+    }
+    public function User11(Request $request){     
+        return view('user11');
+    }
+    public function User12(Request $request){     
+        return view('user12');
+    }
+    public function User13(Request $request){     
+        return view('user13');
+    }
+    public function User14(Request $request){     
+        return view('user14');
+    }
+    public function User15(Request $request){     
+        return view('user15');
+    }
 }
