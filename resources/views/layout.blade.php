33a732658d6e318452e1544d537c97ba5f14e062
kukoza <gglike21@gmail.com>
2025-03-03 09:53:53 +0700

    eqeq

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -24,6 +24,8 @@
       </div>
     </div>
   </nav>
-</div class="container py-2">
+    </div class="container py-2">
+     @yield('content')
+    </div>
   </body>
 </html>
