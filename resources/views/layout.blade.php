142c6bf43f0af30139d0d8cca3b1df5f0c8cb6ed
kukoza <gglike21@gmail.com>
2025-03-03 09:49:27 +0700

    กไ

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,14 +8,17 @@
   </head>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">NZT doc wepp</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="menu">Menu</a>
+            <a class="nav-link active" aria-current="page" href="menu">vCARD</a>
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">ใบขอใช้รถ</a>
           </li> 
         </form>
       </div>
